5a192164b3ad2df368ed4885ee43b2ad1d5ad02d
xurm0vi4 <zarsasho@gmail.com>
2024-07-26 19:23:29 +0300

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import fs from 'fs';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
@@ -16,6 +17,9 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
     cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
